c4ab9d1726e7937b2186269fc1d2693a283ce22a
Shaik Aneef <74362324+shaikaneef@users.noreply.github.com>
2020-12-15 21:00:38 +0530

    Add files via upload

---
+++ qrcode.py
@@ -0,0 +1,6 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+qrcode_string="https://www.instagram.com/shaik_aneef_ani"
+url=pyqrcode.create(qrcode_string)
+url.png("myqrcode.png",scale=15)
